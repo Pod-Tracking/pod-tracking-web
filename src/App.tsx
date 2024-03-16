2a48228d6aeefebd4083c97e0887f1419103136c
Goyo Vargas <gregory.vargas18@gmail.com>
2024-03-16 14:48:17 -0500

    Add basic functionality for searching commanders

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
+import { Input } from './components/ui/input'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState<string>('')
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserInput(event.target.value)
+  }
+
+  const fetchCommanders = async (name: string) => {
+    const response = await fetch(
+      `https://api.magicthegathering.io/v1/cards?name=${name}`
+    )
+    const resultsPromise = response.json()
+    const results = await Promise.resolve(resultsPromise)
+    console.log(results)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Input value={userInput} onChange={handleOnChange} />
+      <button onClick={() => fetchCommanders(userInput)}>Search</button>
     </>
   )
 }
